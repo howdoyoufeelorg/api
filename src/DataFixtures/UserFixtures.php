49685cc847d62f478ed23144a40c8f243f2052ea
Viktor Kostadinov <viktor.kostadinov@gmail.com>
2020-05-07 11:47:50 +0200

    Fixtures update - add editable States and Areas to the test editor

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Area;
+use App\Entity\State;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,6 +14,14 @@ class UserFixtures extends Fixture
     public const EDITOR_USER_REFERENCE = 'editor-user';
 
     private $passwordEncoder;
+
+    private $areas = [
+        'Michigan 48', 'Texas 78'
+    ];
+
+    private $states = [
+        'Michigan', 'Texas'
+    ];
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
@@ -34,9 +44,24 @@ class UserFixtures extends Fixture
         $user->setFirstname('Editor');
         $user->setLastname('Test');
         $user->setRoles([User::ROLE_EDITOR]);
+        foreach($this->states as $stateName) {
+            $state = $manager->getRepository(State::class)->findOneBy(['name' => $stateName]);
+            $user->addState($state);
+        }
+        foreach($this->areas as $areaName) {
+            $area = $manager->getRepository(Area::class)->findOneBy(['name' => $areaName]);
+            $user->addArea($area);
+        }
         $manager->persist($user);
         $this->addReference(self::EDITOR_USER_REFERENCE, $user);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UsGeoEntitiesFixtures::class
+        ];
     }
 }
